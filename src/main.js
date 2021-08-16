baeef683b4aa5c2f1b6653434eedb7ad487958e7
Lesan Alexandru <lesanalexandru14@gmail.com>
2021-08-16 15:59:42 +0300

    installed element ui + lodash

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import ElementUI from 'element-ui'
+import _ from 'lodash';
+Vue.prototype._ = _;
 
-Vue.config.productionTip = false
+Vue.use(ElementUI);
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
