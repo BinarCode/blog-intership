e061be137b52f13c8f1f380787802c724b704173
Alexandru-Stefan <alex@Alexandru-Stefans-MacBook-Pro.local>
2021-08-17 11:56:53 +0300

    Updated i18n.js file

--- src/i18n.js
+++ src/i18n.js
@@ -1,23 +1,15 @@
-import Vue from 'vue'
 import VueI18n from 'vue-i18n'
+import Vue from 'vue'
+import enLocaleFile from '@/assets/lang/en-US.json'
 
-Vue.use(VueI18n)
-
-function loadLocaleMessages () {
-  const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
-  const messages = {}
-  locales.keys().forEach(key => {
-    const matched = key.match(/([A-Za-z0-9-_]+)\./i)
-    if (matched && matched.length > 1) {
-      const locale = matched[1]
-      messages[locale] = locales(key)
-    }
+export default function ({ app }) {
+  Vue.use(VueI18n)
+  app.i18n = new VueI18n({
+    messages: {
+      en: enLocaleFile
+    },
+    locale: 'en',
+    fallbackLocale: 'en',
+    silentTranslationWarn: true
   })
-  return messages
 }
-
-export default new VueI18n({
-  locale: process.env.VUE_APP_I18N_LOCALE || 'en',
-  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
-  messages: loadLocaleMessages()
-})
