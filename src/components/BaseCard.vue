a0c45bd13fbfb4dd5c2662ea78ea043cc1e360ad
Alexandru-Stefan <alex@Alexandru-Stefans-MacBook-Pro.local>
2021-08-17 16:47:09 +0300

    Updated card and container

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -1,10 +1,81 @@
 <template>
-$END$
+  <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
+    <div class="flex-shrink-0">
+      <img class="h-48 w-full object-cover" :src="img.href" alt="Blog post photo">
+    </div>
+    <div class="flex-1 bg-white p-6 flex flex-col justify-between">
+      <div class="flex-1">
+        <p class="text-sm font-medium text-indigo-600">
+          <a
+              v-for="tag in post.tags"
+              :key="tag"
+              href="#"
+              class="hover:underline mr-2">{{ tag }}</a>
+        </p>
+        <a href="#" class="block mt-2">
+          <p class="text-xl font-semibold text-gray-900">
+            {{ post.title }}
+          </p>
+          <p class="mt-3 text-base text-gray-500">
+            {{ post.content }}
+          </p>
+        </a>
+      </div>
+      <div class="mt-6 flex items-center">
+        <div class="flex-shrink-0">
+          <a href="#">
+            <span class="sr-only">{{ author.name }}</span>
+            <img class="h-10 w-10 rounded-full" :src="author.avatar" alt="Author avatar">
+          </a>
+        </div>
+        <div class="ml-3">
+          <p class="text-sm font-medium text-gray-900">
+            <a href="#" class="hover:underline">
+              {{ author.name }}
+            </a>
+          </p>
+          <div class="flex space-x-1 text-sm text-gray-500">
+            <time datetime="2020-03-16">
+              Mar 16, 2020
+            </time>
+            <span aria-hidden="true">
+                &middot;
+              </span>
+            <span>
+                {{ post.views }} views
+              </span>
+            <span aria-hidden="true">
+                &middot;
+              </span>
+            <span>
+                6 min read
+              </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-name: "BaseCard"
+  name: "BaseCard",
+  props: {
+    img: {
+      href: String
+    },
+    post: {
+      link: String,
+      title: String,
+      content: String,
+      tags: Array,
+      views: Number
+    },
+    author: {
+      name: String,
+      avatar: String
+    }
+  }
 }
 </script>
 
--- src/components/BlogsContainer.vue
+++ src/components/BlogsContainer.vue
@@ -1,10 +1,10 @@
 <template>
-$END$
+  <div class="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none"><slot>No posts are available at this moment.</slot></div>
 </template>
 
 <script>
 export default {
-name: "BlogsContainer"
+  name: "BlogsContainer"
 }
 </script>
 
