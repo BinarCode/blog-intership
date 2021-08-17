bad0b39ec931ac6887f6d2eacb732f40652f1a07
mihai <pascu.ioanmihai@gmail.com>
2021-08-17 15:37:16 +0300

    BI-37 - Fix i18n import error

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import './plugins/globalComponents';
-import i18n from './i18n'
+import i18n from '@/plugins/vue-i18n';
 
 Vue.config.productionTip = false;
 
---
+++ src/plugins/vue-i18n.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import VueI18n from 'vue-i18n';
+import enLocaleFile from '@/locales/en.json'
+
+Vue.use(VueI18n);
+
+export default new VueI18n({
+    messages: {
+        en: enLocaleFile
+    },
+    locale: 'en',
+    fallbackLocale: 'en',
+    silentFallbackWarn: process.env.NODE_ENV === 'test',
+    silentTranslationWarn: process.env.NODE_ENV === 'test',
+});
