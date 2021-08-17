643ff39e40c4fb100bcda321297ec127f6c165cd
lucian-popp <lucian.popp@evozon.com>
2021-08-17 10:20:13 +0300

    BI-43 tailwind check

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './assets/tailwind.css'
 
 
 Vue.config.productionTip = false;
-import './assets/tailwind.css'
+
 
 new Vue({
   render: h => h(App),
