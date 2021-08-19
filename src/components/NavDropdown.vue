085f1d4f0f345b1409cc46a2e16dc0c3e4b2e3b2
Alexandru-Stefan <alex.badragan@binarcode.com>
2021-08-19 16:08:37 +0300

    Created NavDropdown component

---
+++ src/components/NavDropdown.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-dropdown trigger="click" class="p-2">
+      <span class="el-dropdown-link flex items-center">
+        <img class="h-5 rounded-full bg-black" src="https://t4.ftcdn.net/jpg/03/46/93/61/360_F_346936114_RaxE6OQogebgAWTalE1myseY1Hbb5qPM.jpg" alt="Avatar">My User Name<i class="el-icon-arrow-down el-icon--right"></i>
+      </span>
+    <el-dropdown-menu slot="dropdown">
+      <el-dropdown-item>Action 1</el-dropdown-item>
+      <el-dropdown-item>Action 2</el-dropdown-item>
+      <el-dropdown-item>Action 3</el-dropdown-item>
+      <el-dropdown-item disabled>Action 4</el-dropdown-item>
+      <el-dropdown-item divided>Action 5</el-dropdown-item>
+    </el-dropdown-menu>
+  </el-dropdown>
+</template>
+
+<script>
+export default {
+  name: "BaseNavDropdown"
+}
+</script>
+
+<style scoped>
+
+</style>
