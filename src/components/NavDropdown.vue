471e5b539d43eacf6777db791953cc79bc689fbb
Alexandru-Stefan <alex.badragan@binarcode.com>
2021-08-19 16:56:11 +0300

    Updated NavDropdown

--- src/components/NavDropdown.vue
+++ src/components/NavDropdown.vue
@@ -4,11 +4,9 @@
         <img class="h-5 rounded-full bg-black mr-1" src="https://t4.ftcdn.net/jpg/03/46/93/61/360_F_346936114_RaxE6OQogebgAWTalE1myseY1Hbb5qPM.jpg" alt="Avatar">My User Name<i class="el-icon-arrow-down el-icon--right"></i>
       </span>
     <el-dropdown-menu slot="dropdown">
-      <el-dropdown-item>Action 1</el-dropdown-item>
-      <el-dropdown-item>Action 2</el-dropdown-item>
-      <el-dropdown-item>Action 3</el-dropdown-item>
-      <el-dropdown-item disabled>Action 4</el-dropdown-item>
-      <el-dropdown-item divided>Action 5</el-dropdown-item>
+      <el-dropdown-item><router-link to="/">Profile</router-link></el-dropdown-item>
+      <el-dropdown-item>My articles</el-dropdown-item>
+      <el-dropdown-item divided>Log out</el-dropdown-item>
     </el-dropdown-menu>
   </el-dropdown>
 </template>
