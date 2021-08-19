f99339d1c3b0ef0ec433bcd854737c584b80f3be
Lesan Alexandru <lesanalexandru14@gmail.com>
2021-08-19 16:49:25 +0300

    integrate BaseButton

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,23 +1,30 @@
 <template>
- <button
-    class="inline-flex items-center font-medium rounded-md shadow-sm text-white" type="button"
-   :class="{
-    'cursor-not-allowed': disabled,
-    'px-3 py-2 text-sm': size === 'sm',
-    'px-4 py-2 text-base': size === 'md',
-    'px-6 py-3 text-base': size === 'lg',
+  <button
+    class="inline-flex items-center font-medium text-white rounded-md shadow-sm"
+    type="button"
+    :class="{
+      'cursor-not-allowed': disabled,
+      'px-3 py-2 text-sm': size === 'sm',
+      'px-4 py-2 text-base': size === 'md',
+      'px-6 py-3 text-base': size === 'lg',
 
-    'bg-indigo-600 bg-opacity-70': color === 'primary' && disabled,
-    'text-indigo-600 bg-white border border-indigo-600': color === 'primary' && outline,
-    'bg-indigo-600 active:bg-indigo-800': color === 'primary' && !disabled && !outline,
+      'bg-indigo-600 bg-opacity-70': color === 'primary' && disabled,
+      'text-indigo-600 bg-white border border-indigo-600':
+        color === 'primary' && outline,
+      'bg-indigo-600 active:bg-indigo-800':
+        color === 'primary' && !disabled && !outline,
 
-    'bg-green-600 bg-opacity-70': color === 'secondary' && disabled,
-    'text-green-600 bg-white border border-green-600': color === 'secondary' && outline,
-    'bg-green-600 active:bg-green-800': color === 'secondary' && !disabled && !outline,
+      'bg-green-600 bg-opacity-70': color === 'secondary' && disabled,
+      'text-green-600 bg-white border border-green-600':
+        color === 'secondary' && outline,
+      'bg-green-600 active:bg-green-800':
+        color === 'secondary' && !disabled && !outline,
     }"
     v-bind="$attrs"
-    v-on="$listeners" >
-    <slot>{{label}}</slot>
+    v-on="$listeners"
+    :disabled="disabled"
+  >
+    <slot>{{ label }}</slot>
   </button>
 </template>
 
@@ -26,38 +33,38 @@ export default {
   name: 'BaseButton',
   props: {
     size: {
-        type: String,
-        default: 'md' //sm|md|lg
-     },
-     color: {
-       type: String,
-       default: 'primary' //primary|secondary
-     },
-     label: {
-       type: String,
-       default: ''
-     },
-     disabled: {
-       type: Boolean,
-       default: false
-     },
-     outline: {
-       type: Boolean,
-       default: false
-     }
+      type: String,
+      default: 'md', //sm|md|lg
     },
-    computed: {
-      listeners() {
-        return {
-          ...this.$listeners,
-          click: this.onClick,
-        }
-      }
+    color: {
+      type: String,
+      default: 'primary', //primary|secondary
     },
-    methods: {
-      onClick(event) {
-        this.$emit('clicked', event)
-      }
-    }
-  }
+    label: {
+      type: String,
+      default: '',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    outline: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    listeners() {
+      return {
+        ...this.$listeners,
+        click: this.onClick,
+      };
+    },
+  },
+  methods: {
+    onClick(event) {
+      this.$emit('clicked', event);
+    },
+  },
+};
 </script>
