20cee583f2c93ceeb7a9ec8123e94952420e51cf
lucian-popp <lucian.popp@evozon.com>
2021-08-19 11:26:07 +0300

    updated button styles

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,6 +1,23 @@
 <template>
- <button type="button" :class="[size, backgroundColor]" >Trimite
-     <slot/>
+ <button
+    class="inline-flex items-center font-medium rounded-md shadow-sm text-white" type="button"
+   :class="{
+    'cursor-not-allowed': disabled,
+    'px-3 py-2 text-sm': size === 'sm',
+    'px-4 py-2 text-base': size === 'md',
+    'px-6 py-3 text-base': size === 'lg',
+
+    'bg-indigo-600 bg-opacity-70': color === 'primary' && disabled,
+    'text-indigo-600 bg-white border border-indigo-600': color === 'primary' && outline,
+    'bg-indigo-600 active:bg-indigo-800': color === 'primary' && !disabled && !outline,
+
+    'bg-green-600 bg-opacity-70': color === 'secondary' && disabled,
+    'text-green-600 bg-white border border-green-600': color === 'secondary' && outline,
+    'bg-green-600 active:bg-green-800': color === 'secondary' && !disabled && !outline,
+    }"
+    v-bind="$attrs"
+    v-on="$listeners" >
+    <slot>{{label}}</slot>
   </button>
 </template>
 
@@ -10,32 +27,37 @@ export default {
   props: {
     size: {
         type: String,
-        default: 'button'
+        default: 'md' //sm|md|lg
      },
-     backgroundColor: {
+     color: {
        type: String,
-       default: 'primary'
+       default: 'primary' //primary|secondary
+     },
+     label: {
+       type: String,
+       default: ''
+     },
+     disabled: {
+       type: Boolean,
+       default: false
+     },
+     outline: {
+       type: Boolean,
+       default: false
      }
+    },
+    computed: {
+      listeners() {
+        return {
+          ...this.$listeners,
+          click: this.onClick,
+        }
+      }
+    },
+    methods: {
+      onClick(event) {
+        this.$emit('clicked', event)
+      }
     }
   }
 </script>
-
-<style >
-
-  .button {
-    @apply inline-flex items-center border border-transparent font-medium rounded-md shadow-sm text-white bg-indigo-600 active:bg-indigo-800 disabled:opacity-50
-  }
-  .sm {
-    @apply  px-3 py-2 text-sm;
-  }
-  .md {
-    @apply  px-4 py-2  text-base;
-  }
-  .lg {
-    @apply  px-6 py-3  text-base;
-  }
-  .primary {
-    @apply bg-indigo-600
-  }
-
-</style>
--- src/views/Styleguide.vue
+++ src/views/Styleguide.vue
@@ -1,12 +1,14 @@
 <template>
-<div>
-  <div>{{ $t('styleguide') }}!</div>
-  <p class="text-red-300 font-bold">styleguide</p>
+<div class="p-20">
+  <h1 class="text-5xl font-bold my-4">BaseButtonStyles</h1>
+  <base-button-styles></base-button-styles>
   </div>
 </template>
 
 <script>
+import BaseButtonStyles from '../components/styles/BaseButtonStyles.vue'
 export default {
+  components: { BaseButtonStyles },
   name: 'Styleguide'
 }
 </script>
