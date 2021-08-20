b68ae2c7848d099082e1d50c0fae3ff454017a28
Alexandru-Stefan <alex.badragan@binarcode.com>
2021-08-20 11:16:34 +0300

    Updated route method

--- src/components/NavDropdown.vue
+++ src/components/NavDropdown.vue
@@ -1,32 +1,29 @@
 <template>
   <el-dropdown trigger="click" class="p-2">
       <span class="el-dropdown-link flex items-center">
-        <img class="h-5 rounded-full bg-black mr-1" :src="avatar || 'https://t4.ftcdn.net/jpg/03/46/93/61/360_F_346936114_RaxE6OQogebgAWTalE1myseY1Hbb5qPM.jpg'" alt="Avatar">@{{ username }}<i class="el-icon-arrow-down el-icon--right"></i>
+        <img class="h-5 rounded-full bg-black mr-1" :src="avatar || 'https://t4.ftcdn.net/jpg/03/46/93/61/360_F_346936114_RaxE6OQogebgAWTalE1myseY1Hbb5qPM.jpg'" alt="Avatar">
+        @{{ username }}
+        <i class="el-icon-arrow-down el-icon--right"></i>
       </span>
     <el-dropdown-menu slot="dropdown">
       <el-dropdown-item @click.native="profile">{{ $t('general.profile.title') }}</el-dropdown-item>
-      <el-dropdown-item @click.native="myArticles">{{ $t('general.profile.myArticles') }}</el-dropdown-item>
-      <el-dropdown-item @click.native="logout" divided>{{ $t('general.profile.logout') }}</el-dropdown-item>
+      <el-dropdown-item>{{ $t('general.profile.myArticles') }}</el-dropdown-item>
+      <el-dropdown-item divided>{{ $t('general.profile.logout') }}</el-dropdown-item>
     </el-dropdown-menu>
   </el-dropdown>
 </template>
 
 <script>
 export default {
-  name: "BaseNavDropdown",
+  name: 'BaseNavDropdown',
   props: {
     username: String,
     avatar: String
   },
   methods: {
     profile() {
-      this.$router.push('/profile');
-    },
-    myArticles() {
-      this.$router.push(`/`);
-    },
-    logout() {
-      this.$router.push(`/`);
+      if (this.$route.name != "Profile")
+        this.$router.push('/profile');
     }
   }
 }
