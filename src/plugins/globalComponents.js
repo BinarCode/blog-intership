82d7b4ee9a046066e68d536c5ac6033bb0626ca8
Alexandru-Stefan <alex.badragan@binarcode.com>
2021-08-23 10:48:43 +0300

    Added BaseButton as global component

--- src/plugins/globalComponents.js
+++ src/plugins/globalComponents.js
@@ -1,6 +1,7 @@
 import 'element-ui/lib/theme-chalk/index.css';
 import { Button, Dropdown, Select } from 'element-ui';
 import BaseInput from '@/components/BaseInput.vue';
+import BaseButton from '@/components/BaseButton';
 
 
 export default {
@@ -9,6 +10,7 @@ export default {
         Vue.component('el-dropdown', Dropdown);
         Vue.component('el-select', Select);
         Vue.component('base-input', BaseInput);
+        Vue.component('base-button', BaseButton);
     },
 }
 
