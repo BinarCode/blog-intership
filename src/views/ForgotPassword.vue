9dfb0afcc23d55c11917b258a5e5854ad01b0500
Alexandru-Stefan <alex.badragan@binarcode.com>
2021-08-23 11:04:23 +0300

    Resolved comments

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -27,16 +27,12 @@
                 type="submit"
                 class="flex justify-center w-full px-4 py-2 my-4 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 :disabled="!valid"
+                :loading="loading"
             >
               {{ $t('general.submit') }}
             </base-button>
           </form>
         </ValidationObserver>
-        <img
-          v-if="loading"
-          class="h-5 w-5 mx-auto my-3"
-          src="https://icon-library.com/images/loading-icon-transparent-background/loading-icon-transparent-background-12.jpg"
-        >
       </div>
       </div>
     </div>
