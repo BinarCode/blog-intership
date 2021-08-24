c855849d5e04e493bc9edf9fb344b8f1aa395d5c
Alexandru-Stefan <alex.badragan@binarcode.com>
2021-08-24 09:29:06 +0300

    Removed

--- src/components/MenuDropdown.vue
+++ src/components/MenuDropdown.vue
@@ -29,10 +29,10 @@ export default {
   },
   computed: {
     getAvatar() {
-      if (!this.$props.avatar)
+      if (!this.avatar)
         return 'https://t4.ftcdn.net/jpg/03/46/93/61/360_F_346936114_RaxE6OQogebgAWTalE1myseY1Hbb5qPM.jpg';
 
-      return this.$props.avatar;
+      return this.avatar;
     }
   },
   methods: {
