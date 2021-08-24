cc4712a1eec4fae88d698071afcd5b193170538e
Lesan Alexandru <lesanalexandru14@gmail.com>
2021-08-24 14:21:35 +0300

    pull from master+ resolved 2 comm from middlewsre

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -2,18 +2,18 @@
   <button
     class="flex items-center justify-center mt-1 font-medium text-white rounded-md shadow-sm"
     :class="{
-      'cursor-not-allowed': disabledOrloading,
+      'cursor-not-allowed': isDisabled,
       'px-3 py-2 text-sm': size === 'sm',
       'px-4 py-2 text-base': size === 'md',
       'px-6 py-3 text-base': size === 'lg',
 
-      'bg-indigo-600 bg-opacity-70': color === 'primary' && disabledOrloading,
+      'bg-indigo-600 bg-opacity-70': color === 'primary' && isDisabled,
       'text-indigo-600 bg-white border border-indigo-600':
         color === 'primary' && outline,
       'bg-indigo-600 active:bg-indigo-800':
         color === 'primary' && !disabled && !outline,
 
-      'bg-green-600 bg-opacity-70': color === 'secondary' && disabledOrloading,
+      'bg-green-600 bg-opacity-70': color === 'secondary' && isDisabled,
       'text-green-600 bg-white border border-green-600':
         color === 'secondary' && outline,
       'bg-green-600 active:bg-green-800':
@@ -22,7 +22,7 @@
     :type="type"
     v-bind="$attrs"
     v-on="$listeners"
-    :disabled="disabledOrloading"
+    :disabled="isDisabled"
   >
     <i v-if="loading" class="text-lg leading-none el-icon-loading" />
     <div class="mx-2">
@@ -71,7 +71,7 @@ export default {
         click: this.onClick,
       };
     },
-    disabledOrloading() {
+    isDisabled() {
       return this.disabled || this.loading;
     },
   },
