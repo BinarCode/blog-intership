f076b8c8a5ea17b761541646a6f6a3680629fc23
Alexandru-Stefan <alex.badragan@binarcode.com>
2021-08-25 12:44:55 +0300

    Created BaseNotification component

---
+++ src/components/BaseNotification.vue
@@ -0,0 +1,40 @@
+<script>
+import { Notification } from 'element-ui';
+
+export default {
+  name: 'BaseNotification',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    message: {
+      type: String,
+      required: true,
+      default: ''
+    },
+    type: {
+      type: String, // success |  warning | info | error
+      required: true,
+      default: 'info'
+    },
+    autoClose: {
+      type: Boolean,
+      default: true
+    }
+  },
+  mounted() {
+    Notification({
+      title: this.$props.title,
+      message: this.$props.message,
+      type: this.$props.type,
+      duration: this.$props.autoClose ? undefined : 0
+    });
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
