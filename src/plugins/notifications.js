b640b67923a7cc751ff8a4b474750b0e903bb951
Alexandru-Stefan <alex.badragan@binarcode.com>
2021-08-26 15:49:53 +0300

    Added default duration

--- src/plugins/notifications.js
+++ src/plugins/notifications.js
@@ -2,13 +2,13 @@ import {Notification} from "element-ui";
 
 export default {
     install(Vue) {
-        Vue.prototype.$notify = (title, message, type, position = 'top-right', duration = undefined) => {
+        Vue.prototype.$notify = (title, message, type, position = 'top-right', duration = 4500) => {
             Notification({
                 title: title,
                 message: message,
                 type: type, // 'success' | 'error' | 'warning' | 'info'
                 position: position, // 'top-right' | 'bottom-right' | 'top-left' | 'bottom-left'
-                duration: duration // undefined (closing automatically) | 0 (closing manually)
+                duration: duration // number of milliseconds | 0 (closing manually)
             });
         }
     }
