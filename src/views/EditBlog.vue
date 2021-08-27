1a9fb09c212b2234d6e0b8fa7665ba475b72d5ab
Lesan Alexandru <lesanalexandru14@gmail.com>
2021-08-27 10:40:12 +0300

    initiate

---
+++ src/views/EditBlog.vue
@@ -0,0 +1,24 @@
+<template></template>
+
+<script>
+export default {
+  name: 'EditBlog',
+  data() {
+    return {
+      model: {
+        title: '',
+        author: '',
+        publishDate: '',
+        categories: '',
+        tags: '',
+        summary: '',
+        image: '',
+        imageAlt: '',
+        content: '',
+      },
+    };
+  },
+};
+</script>
+
+<style scoped></style>
