3c8b9d10a171bfedb8d62bae3cef6f4dac84a82a
Alexandru-Stefan <alex.badragan@binarcode.com>
2021-08-27 09:51:41 +0300

    Updated notifications plugin

--- src/plugins/notifications.js
+++ src/plugins/notifications.js
@@ -2,13 +2,13 @@ import {Notification} from "element-ui";
 
 export default {
     install(Vue) {
-        Vue.prototype.$notify = (title, message, type, position = 'top-right', duration = 4500) => {
+        Vue.prototype.$notify = (notifications) => {
             Notification({
-                title: title,
-                message: message,
-                type: type, // 'success' | 'error' | 'warning' | 'info'
-                position: position, // 'top-right' | 'bottom-right' | 'top-left' | 'bottom-left'
-                duration: duration // number of milliseconds | 0 (closing manually)
+                title: notifications.title,
+                message: notifications.message,
+                type: notifications.type, // 'success' | 'error' | 'warning' | 'info'
+                position: notifications.position || 'top-left', // 'top-right' | 'bottom-right' | 'top-left' | 'bottom-left'
+                duration: notifications.duration || 4500 // number of milliseconds | 0 (closing manually)
             });
         }
     }
