0a74b764054af3f999aaf1342ebf45822e9504ce
Alexandru-Stefan <alex.badragan@binarcode.com>
2021-08-27 16:34:24 +0300

    Created Profile view

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,10 +1,245 @@
 <template>
-  <div></div>
+  <div>
+    <div>
+      <div class="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="sm:flex sm:items-end sm:space-x-5">
+          <div class="flex">
+            <img class="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32" src="https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80" alt="">
+            <span v-if="edit" class="absolute mt-24 sm:mt-32">
+              <a href="#" class="text-blue-500 hover:underline md:ml-4">upload</a>&nbsp;
+              <a href="#" class="text-red-500 hover:underline">clear</a>
+            </span>
+          </div>
+          <div class="mt-6 sm:flex-1 sm:min-w-0 sm:flex sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
+            <div class="mt-6 flex flex-col justify-stretch space-y-3 sm:flex-row sm:space-y-0 sm:space-x-4">
+              <base-button v-if="!edit" size="sm" @click="edit = !edit">
+                Edit profile
+              </base-button>
+              <base-button v-if="edit" size="sm">
+                Save
+              </base-button>
+              <base-button v-if="edit" size="sm" @click="edit = !edit">
+                Cancel
+              </base-button>
+            </div>
+          </div>
+        </div>
+        <div class="text-2xl sm:block mt-6 min-w-0 flex-1">
+          <span v-if="!edit">{{ user.first_name }} {{ user.last_name }}</span>
+          <div v-else>
+            <base-input :label="$t('register.name.firstName')" :value="user.first_name"></base-input>
+            <base-input :label="$t('register.name.firstName')" :value="user.last_name"></base-input>
+          </div>
+        </div>
+      </div>
+    </div>
+    <hr class="mt-2"/>
+    <!-- Description list -->
+    <div class="mt-6 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+      <dl class="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+        <div class="sm:col-span-1">
+          <dt class="text-sm font-medium text-gray-500">
+            Email
+          </dt>
+          <dd class="mt-1 text-sm text-gray-900">
+            {{ user.email }}
+          </dd>
+        </div>
+
+        <div class="sm:col-span-1">
+          <dt class="text-sm font-medium text-gray-500">
+            Last updated at
+          </dt>
+          <dd class="mt-1 text-sm text-gray-900">
+            {{ user.updated_at }}
+          </dd>
+        </div>
+
+        <div class="sm:col-span-2">
+          <dt class="text-sm font-medium text-gray-500">
+            About
+          </dt>
+          <dd class="mt-1 max-w-prose text-sm text-gray-900 space-y-5">
+            <p>Tincidunt quam neque in cursus viverra orci, dapibus nec tristique. Nullam ut sit dolor consectetur urna, dui cras nec sed. Cursus risus congue arcu aenean posuere aliquam.</p>
+            <p>Et vivamus lorem pulvinar nascetur non. Pulvinar a sed platea rhoncus ac mauris amet. Urna, sem pretium sit pretium urna, senectus vitae. Scelerisque fermentum, cursus felis dui suspendisse velit pharetra. Augue et duis cursus maecenas eget quam lectus. Accumsan vitae nascetur pharetra rhoncus praesent dictum risus suspendisse.</p>
+          </dd>
+        </div>
+      </dl>
+    </div>
+
+    <!-- Latest comments -->
+    <div class="mt-8 max-w-5xl mx-auto px-4 pb-12 sm:px-6 lg:px-8">
+      <h2 class="text-sm font-medium text-gray-500">Latest comments</h2>
+      <div class="mt-1 grid grid-cols-1 gap-4 sm:grid-cols-2">
+        <div class="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-pink-500">
+          <div class="flex-shrink-0">
+            <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+          </div>
+          <div class="flex-1 min-w-0">
+            <a href="#" class="focus:outline-none">
+              <span class="absolute inset-0" aria-hidden="true"></span>
+              <p class="text-sm font-medium text-gray-900">
+                Leslie Alexander
+              </p>
+              <p class="text-sm text-gray-500 truncate">
+                Co-Founder / CEO
+              </p>
+            </a>
+          </div>
+        </div>
+
+        <div class="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-pink-500">
+          <div class="flex-shrink-0">
+            <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+          </div>
+          <div class="flex-1 min-w-0">
+            <a href="#" class="focus:outline-none">
+              <span class="absolute inset-0" aria-hidden="true"></span>
+              <p class="text-sm font-medium text-gray-900">
+                Michael Foster
+              </p>
+              <p class="text-sm text-gray-500 truncate">
+                Co-Founder / CTO
+              </p>
+            </a>
+          </div>
+        </div>
+
+        <div class="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-pink-500">
+          <div class="flex-shrink-0">
+            <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+          </div>
+          <div class="flex-1 min-w-0">
+            <a href="#" class="focus:outline-none">
+              <span class="absolute inset-0" aria-hidden="true"></span>
+              <p class="text-sm font-medium text-gray-900">
+                Dries Vincent
+              </p>
+              <p class="text-sm text-gray-500 truncate">
+                Manager, Business Relations
+              </p>
+            </a>
+          </div>
+        </div>
+
+        <div class="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-pink-500">
+          <div class="flex-shrink-0">
+            <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+          </div>
+          <div class="flex-1 min-w-0">
+            <a href="#" class="focus:outline-none">
+              <span class="absolute inset-0" aria-hidden="true"></span>
+              <p class="text-sm font-medium text-gray-900">
+                Lindsay Walton
+              </p>
+              <p class="text-sm text-gray-500 truncate">
+                Front-end Developer
+              </p>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Latest blogs -->
+    <div class="mt-8 max-w-5xl mx-auto px-4 pb-12 sm:px-6 lg:px-8">
+      <h2 class="text-sm font-medium text-gray-500">Latest blogs</h2>
+      <div class="mt-1 grid grid-cols-1 gap-4 sm:grid-cols-2">
+        <div class="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-pink-500">
+          <div class="flex-shrink-0">
+            <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+          </div>
+          <div class="flex-1 min-w-0">
+            <a href="#" class="focus:outline-none">
+              <span class="absolute inset-0" aria-hidden="true"></span>
+              <p class="text-sm font-medium text-gray-900">
+                Leslie Alexander
+              </p>
+              <p class="text-sm text-gray-500 truncate">
+                Co-Founder / CEO
+              </p>
+            </a>
+          </div>
+        </div>
+
+        <div class="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-pink-500">
+          <div class="flex-shrink-0">
+            <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+          </div>
+          <div class="flex-1 min-w-0">
+            <a href="#" class="focus:outline-none">
+              <span class="absolute inset-0" aria-hidden="true"></span>
+              <p class="text-sm font-medium text-gray-900">
+                Michael Foster
+              </p>
+              <p class="text-sm text-gray-500 truncate">
+                Co-Founder / CTO
+              </p>
+            </a>
+          </div>
+        </div>
+
+        <div class="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-pink-500">
+          <div class="flex-shrink-0">
+            <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+          </div>
+          <div class="flex-1 min-w-0">
+            <a href="#" class="focus:outline-none">
+              <span class="absolute inset-0" aria-hidden="true"></span>
+              <p class="text-sm font-medium text-gray-900">
+                Dries Vincent
+              </p>
+              <p class="text-sm text-gray-500 truncate">
+                Manager, Business Relations
+              </p>
+            </a>
+          </div>
+        </div>
+
+        <div class="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-pink-500">
+          <div class="flex-shrink-0">
+            <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+          </div>
+          <div class="flex-1 min-w-0">
+            <a href="#" class="focus:outline-none">
+              <span class="absolute inset-0" aria-hidden="true"></span>
+              <p class="text-sm font-medium text-gray-900">
+                Lindsay Walton
+              </p>
+              <p class="text-sm text-gray-500 truncate">
+                Front-end Developer
+              </p>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import {mapGetters} from "vuex";
+
 export default {
-  name: "Profile"
+  name: "Profile",
+
+  data() {
+    return {
+      edit: false
+    }
+  },
+
+  watch: {
+    edit() {
+      console.log(this.user);
+    }
+  },
+
+  computed: {
+    ...mapGetters({
+      user: 'userState'
+    })
+  }
 }
 </script>
 
