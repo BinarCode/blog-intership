0e0269d67982f77489146e95534e815e1ff67fa2
Alexandru-Stefan <alex.badragan@binarcode.com>
2021-08-27 09:55:09 +0300

    Updated default notification position

--- src/plugins/notifications.js
+++ src/plugins/notifications.js
@@ -7,7 +7,7 @@ export default {
                 title: notifications.title,
                 message: notifications.message,
                 type: notifications.type, // 'success' | 'error' | 'warning' | 'info'
-                position: notifications.position || 'top-left', // 'top-right' | 'bottom-right' | 'top-left' | 'bottom-left'
+                position: notifications.position || 'top-right', // 'top-right' | 'bottom-right' | 'top-left' | 'bottom-left'
                 duration: notifications.duration || 4500 // number of milliseconds | 0 (closing manually)
             });
         }
