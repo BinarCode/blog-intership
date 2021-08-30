943bcde5170b92d7e173e15e4f1225546b258ec1
Alexandru-Stefan <alex.badragan@binarcode.com>
2021-08-30 17:27:01 +0300

    Added get profile function

--- src/api/userService.js
+++ src/api/userService.js
@@ -1,6 +1,10 @@
 import axios from '@/api/axiosConfig';
 
 export default {
+    async getProfile() {
+        return await axios.get('/api/restify/profile?related=blogs,comments,media');
+    },
+
     async updateProfile(data) {
         return await axios.put('/api/restify/profile', data);
     },
