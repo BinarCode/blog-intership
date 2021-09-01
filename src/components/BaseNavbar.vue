cb55612b0266dc46a85e58ddf15d47ebdc75ec70
Alexandru-Stefan <alex.badragan@binarcode.com>
2021-09-01 16:41:51 +0300

    Fixed navbar avatar bug

--- src/components/BaseNavbar.vue
+++ src/components/BaseNavbar.vue
@@ -109,6 +109,12 @@ export default {
       return this.user.first_name || this.user.last_name || name
     },
 
+    getAvatar() {
+      return this.user.avatar
+          ? this.user.avatar
+          : 'https://t4.ftcdn.net/jpg/03/46/93/61/360_F_346936114_RaxE6OQogebgAWTalE1myseY1Hbb5qPM.jpg';
+    },
+
     routeName() {
       return this.$route.name;
     }
