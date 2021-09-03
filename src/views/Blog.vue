864f2f973a257464e0e36cb81c1ab9d4fd4a4a79
Alexandru-Stefan <alex.badragan@binarcode.com>
2021-09-03 14:57:38 +0300

    Integrated get blog as PDF

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -12,21 +12,30 @@
         "
         :alt="$t('blog.image.alt')"
       />
-      <div class="tags cursor-pointer flex flex-wrap mx-auto space-x-4">
-        <div
-          class="inline-flex px-4 py-2.5 rounded-full text-sm font-medium bg-blue-100 text-blue-800 mb-2"
-          v-for="(tag, index) in get(blog, 'attributes.tags', [])"
-          :key="index"
-        >
-          {{ get(tag, 'value', '') }}
+      <div ref="blog">
+        <div class="tags cursor-pointer flex flex-wrap mx-auto space-x-4">
+          <div
+            class="inline-flex px-4 py-2.5 rounded-full text-sm font-medium bg-blue-100 text-blue-800 mb-2"
+            v-for="(tag, index) in get(blog, 'attributes.tags', [])"
+            :key="index"
+          >
+            {{ get(tag, 'value', '') }}
+          </div>
         </div>
-      </div>
-      <div class="title text-5xl my-5 text-center w-full">
-        {{ get(blog, 'attributes.title', '') }}
+        <div class="title text-5xl my-5 text-center w-full">
+          {{ get(blog, 'attributes.title', '') }}
+        </div>
+
+        <div class="mb-20" v-html="get(blog, 'attributes.content', '')" />
       </div>
 
-      <div class="mb-20" v-html="get(blog, 'attributes.content', '')" />
+      <base-button @click="getPDF">{{ $t('blog.getAsPDF.text') }}</base-button>
     </div>
+
+    <div class="hidden">
+      <div ref="pdfContent" style="width: 768px"></div>
+    </div>
+
     <vue-scroll-progress-bar
       backgroundColor="linear-gradient(to right, #BFDBFE,#4F46E5)"
       height=".3rem"
@@ -41,6 +50,7 @@ import get from 'lodash/get';
 import { getBlog, addViewOnBlog } from '@/api/blogService';
 import BackToTop from '@/components/BackToTop';
 import { VueScrollProgressBar } from '@guillaumebriday/vue-scroll-progress-bar';
+import jsPDF from 'jspdf';
 
 export default {
   name: 'Blog',
@@ -57,12 +67,37 @@ export default {
       data.attributes.tags = getTagsArray(get(data, 'attributes.tags', ''));
       await addViewOnBlog(this.blogId);
       this.blog = data;
+
     } catch (error) {
       this.notifyErrors(error);
     }
   },
   methods: {
     get,
+    jsPDF,
+    async getPDF() {
+      this.$refs.pdfContent.innerHTML = this.$refs.blog.innerHTML;
+      const page = this.$refs.pdfContent;
+
+      var doc = new jsPDF({
+        orientation: "p",
+        format: "a4",
+        unit: "px"
+      });
+
+      await doc.html(page, {
+        html2canvas: {
+          scale: 0.54
+        },
+        callback: function (doc) {
+          doc.save(`${get(this.blog, 'attributes.title', 'blog')}.pdf`);
+        },
+        x: 15,
+        y: 20
+      });
+
+      this.$refs.pdfContent.innerHTML = "";
+    }
   },
 };
 </script>
